1f26e832648369b8f7192010f17748b35ea832d6
abhishek <abhishekkatnoria993@gmail.com>
2024-10-03 17:04:25 +0530

    Added the Governance Section

---
+++ src/Components/HomePage/Governance/index.tsx
@@ -0,0 +1,46 @@
+import { GovernanceData } from "../../../json";
+import Button from "../../Button";
+
+const Governance = () => {
+  console.log(GovernanceData?.cards);
+  return (
+    <div className="my-[180px]">
+      <div className="mb-[120px]">
+        <h2>{GovernanceData?.heading}</h2>
+      </div>
+      <div className="flex gap-6 ">
+        {GovernanceData?.cards.map((item, i) => (
+          <div
+            className="flex justify-center items-center flex-col gap-[18px] py-[42px] pl-[14px] pr-[14px] rounded-xl bg-backgroundBlack gradient-border"
+            key={i}
+          >
+            <div className="inline-block">
+              <img src={item?.logo} alt="Cards" />
+            </div>
+
+            <h3 className="text-font5xl">{item?.name}</h3>
+            <p>{item?.desc}</p>
+
+            <div className="flex gap-[11px]">
+              {
+                item?.btn?.map((items, index) => (
+                  <div key={index} className="flex justify-center items-center gap-[11px]">
+                    {items?.label ? (
+                      <Button variant="filled" css="rounded-[42px]">{items?.label}</Button>
+                    ) : (
+                      <Button variant="filled" css="rounded-[42px]">
+                        <img src={items?.icon} alt="Button icon" className="p-[5px]" />
+                      </Button>
+                    )}
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Governance;
