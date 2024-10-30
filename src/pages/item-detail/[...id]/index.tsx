295f2488468e68df3b5a443e33bed61582b9ca2e
Abhishek katnoria <abhishekkatnoria993@gmail.com>
2024-10-30 13:05:43 +0530

    Added the functionality of counter and according to the counter the price update

--- src/pages/item-detail/[...id]/index.tsx
+++ src/pages/item-detail/[...id]/index.tsx
@@ -5,18 +5,32 @@ import { Cards } from "../../../Types";
 const ItemDetail = () => {
   let { id } = useParams();
   const [data, setData] = useState<Cards[] | undefined>(undefined);
+  const [count, setCount] = useState<number>(1);
+  const [price, setPrice] = useState<number>(0);
+
   useEffect(() => {
     const cards = JSON.parse(localStorage.getItem(`likes`) || "[]");
-    console.log(cards);
     if (id) {
       const parsedId = parseInt(id);
       if (!isNaN(parsedId)) {
         const card = cards.filter((item: any) => item.id === parsedId);
         setData(card);
+        let buyPrice = card.map((items: any) => items.price);
+        let prices = parseInt(buyPrice);
+        setPrice(prices);
       }
     }
   }, [id]);
-  console.log(data);
+
+  const handleSub = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="my-[120px] max-w-[1026px] w-full mx-auto text-center">
@@ -31,16 +45,18 @@ const ItemDetail = () => {
       <div className="mt-[100px] flex gap-10 items-center">
         {data?.map((items, i) => (
           <>
-            <img
-              src={items?.img}
-              alt={items?.alt}
-              className="object-cover rounded-[20px]"
-            />
-            <div>
-              <h2 className="text-font5xl leading-lineHeight4xl mb-3">
+            <div className="w-full max-w-[588px] h-[580px]">
+              <img
+                src={items?.img}
+                alt={items?.alt}
+                className="object-cover rounded-[20px] w-full h-full"
+              />
+            </div>
+            <div className="w-full max-w-[467px]">
+              <h2 className="text-font5xl leading-lineHeight4xl mb-3 text-start">
                 {items?.name}
               </h2>
-              <span className="text-fontSm">
+              <span className="text-fontBase mb-[17px] inline-block text-start leading-7 tracking-ab">
                 In our marketplace, we feature a unique collection of NFTs
                 inspired by cyborg alien mechas—futuristic, biomechanical beings
                 that blend advanced technology with extraterrestrial elements.
@@ -51,6 +67,33 @@ const ItemDetail = () => {
                 where technology and alien biology merge into formidable,
                 otherworldly warriors.
               </span>
+              <div className="flex w-full gap-3">
+                <div className="bg-white bg-opacity-20 flex justify-between items-center pt-[18px] pb-[18px] w-full max-w-[148px] rounded-xl pr-5 pl-5">
+                  <img
+                    src="/AiCollectionImages/sub.svg"
+                    alt=""
+                    className="w-[20px] h-[20px] aspect-square"
+                    onClick={handleSub}
+                  />
+                  <p className="text-3xl leading-9 font-inriasans font-bold">
+                    {count}
+                  </p>
+                  <img
+                    src="/AiCollectionImages/add.svg"
+                    alt=""
+                    className="w-[20px] h-[20px] aspect-square"
+                    onClick={handleAdd}
+                  />
+                </div>
+                <div className="w-full max-w-[280px] bg-white bg-opacity-20 rounded-xl flex justify-center items-center gap-3 font-inriasans">
+                  <p className="text-[32px] leading-9 font-inriasans font-bold">
+                    Buy now for
+                  </p>
+                  <p className="text-[32px] leading-9 font-inriasans font-bold">
+                    {count * price}$
+                  </p>
+                </div>
+              </div>
             </div>
           </>
         ))}
