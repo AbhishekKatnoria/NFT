34844f3048254b6ad9307352eedcde372f1185ff
Abhishek katnoria <abhishekkatnoria993@gmail.com>
2024-12-04 17:16:34 +0530

    Add onclick function

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,10 +1,14 @@
 import { ButtonProps } from "../../Types";
 
-const Button = ({ variant, css, label, children }: ButtonProps) => {
+const Button = ({ variant, css, label, children, onClick }: ButtonProps) => {
   return (
     <button
-      className={`flex gap-[10px] py-2 px-3 border-[0.5px] cursor-pointer items-center ${variant === "filled" ? "bg-gradient-filled" : "bg-gradient-transparent gradient-border-varient-third"
-        } ${css}`}
+      className={`flex gap-[10px] py-2 px-3 border-[0.5px] cursor-pointer items-center ${
+        variant === "filled"
+          ? "bg-gradient-filled"
+          : "bg-gradient-transparent gradient-border-varient-third"
+      } ${css}`}
+      onClick={() => (onClick ? onClick() : "")}
     >
       {label} {children}
     </button>
