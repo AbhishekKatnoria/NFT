8cd7ca9f55a6817a7c9563b10ca6b5fdaa6bbb59
Abhishek katnoria <abhishekkatnoria993@gmail.com>
2024-12-18 13:58:07 +0530

    Added the AOS in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ import AiImages from "./pages/aiImage";
 import Community from "./pages/community";
 import MarketPlace from "./pages/market-place";
 import ItemDetail from "./pages/item-detail/[...id]";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: false,
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
